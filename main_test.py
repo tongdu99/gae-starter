53224465d22395ce8f9f9429637c6271004a34c7
Tong Du <tongdu@gmail.com>
2017-01-09 09:21:12 -0800

    no need to use urlfetch for unittest

--- main_test.py
+++ main_test.py
@@ -1,17 +1,13 @@
 import unittest
 import requests
-import requests_toolbelt.adapters.appengine
 
 from google.appengine.ext import testbed
-
-requests_toolbelt.adapters.appengine.monkeypatch()
 
 class MainTestCase(unittest.TestCase):
 
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
-        self.testbed.init_urlfetch_stub()
 
     def tearDown(self):
         self.testbed.deactivate()
