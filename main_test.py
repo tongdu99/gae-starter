dcc0b81f6ce4573e3f6ddb6675f3e76ce5d610ae
Tong Du <tong.du@roche.com>
2017-01-13 15:06:02 -0800

    add selenium test for home page

--- main_test.py
+++ main_test.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+from selenium import webdriver
 
 from google.appengine.ext import testbed
 
@@ -17,3 +18,10 @@ class MainTestCase(unittest.TestCase):
         url = 'http://localhost:8080/api/1.0/helloworld'
         response = requests.get(url)
         self.assertEqual('{"Hello": "World"}\n', response.content)
+
+    def testHome(self):
+        url = 'http://localhost:8080'
+        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        driver.get(url)
+        helloworld = driver.find_element_by_tag_name("p")
+        self.assertEqual('Hello World', helloworld.text)
