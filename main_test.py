beb115d864a2f823b5afb00455aeab2d6eba670a
Tong Du <tongdu@gmail.com>
2017-01-15 00:52:12 -0800

    refactoring unit test; add test for messages

--- main_test.py
+++ main_test.py
@@ -10,19 +10,25 @@ class MainTestCase(unittest.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
+        self.url_base = 'http://127.0.0.1:8080'
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 
     def tearDown(self):
         self.testbed.deactivate()
 
     def testHello(self):
-        url = 'http://localhost:8080/api/1.0/helloworld'
+        url = 'http://127.0.0.1:8080/api/1.0/helloworld'
         response = requests.get(url)
-
         self.assertEqual("Hello World", response.json()['message'])
 
     def testHome(self):
-        url = 'http://localhost:8080'
-        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-        driver.get(url)
-        helloworld = driver.find_element_by_tag_name("p")
+        self.driver.get(self.url_base)
+        helloworld = self.driver.find_element_by_tag_name("p")
         self.assertEqual('Hello World', helloworld.text)
+
+    def testMessages(self):
+        self.driver.get(self.url_base+"/messages")
+        message_elements = self.driver.find_elements_by_tag_name("h2")
+        messages = [msg.text for msg in message_elements]
+        self.assertSequenceEqual(messages, ['Hello City', 'Hello Country', 'Hello World', 'Hello Universe'])
+
