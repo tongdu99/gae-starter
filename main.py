2e0474384b4c8ab09cebf4e91ecfabd735e884f9
Tong Du <tongdu@gmail.com>
2017-01-14 23:45:45 -0800

    add messages to handle list in template

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template
 from flask_restful import Api
 from google.appengine.api import app_identity
 
-from hello.HelloWorld import HelloWorld
+from hello.messages import HelloWorld, HelloMessage
+
 import requests
 from requests_toolbelt.adapters import appengine
 
@@ -20,6 +21,7 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, '/api/1.0/helloworld')
+api.add_resource(HelloMessage, '/api/1.0/hellomessage')
 
 url_prefix = "http://"
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
@@ -28,11 +30,21 @@ if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
 host_name = app_identity.get_default_version_hostname()
 url_base = url_prefix+host_name
 
+
 @app.route('/')
 def home():
     url = url_base + '/api/1.0/helloworld'
+    # message = requests.get(url).json()['message']
+    # return render_template('home.html', message=message)
     response = requests.get(url)
     return render_template('home.html', message=response.json()['message'])
+
+@app.route('/messages')
+def message_list():
+    url = url_base + '/api/1.0/hellomessage'
+    messages = requests.get(url).json()['messages']
+    return render_template('messages.html', messages=messages)
+
 
 @app.errorhandler(500)
 def server_error(e):
